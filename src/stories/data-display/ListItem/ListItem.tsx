232fa13290541a4ea8270f0ca2cab0531c7b2b54
Roni Rozen <50151418+ronirozen@users.noreply.github.com>
2023-07-18 20:43:06 +0300

    Update ListItem.tsx

--- src/stories/data-display/ListItem/ListItem.tsx
+++ src/stories/data-display/ListItem/ListItem.tsx
@@ -1,5 +1,4 @@
 // Type
-import { Divider } from "@mui/material";
 import { ListItemProps } from "./ListItem.type";
 
 // Style
@@ -7,9 +6,8 @@ import { StyledListItem } from "./ListItem.style";
 
 export const ListItem = ({ children, disablePadding, divider }: ListItemProps) => {
   return (
-    <>
-      <StyledListItem disablePadding={disablePadding}>{children}</StyledListItem>
-      {divider && <Divider component="li" role="listitem" />}
-    </>
+    <StyledListItem divider={divider} disablePadding={disablePadding}>
+      {children}
+    </StyledListItem>
   );
 };
